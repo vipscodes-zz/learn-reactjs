4b15a05ad179898b59713f176f6a49f4d5a5de54
vipscodes <vipscodes@gmail.com>
2021-07-25 15:35:27 +0700

    stateless & statefull component

---
+++ src/components/HelloComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./HelloComponent.css";
+// stateless component / functional component
+const HelloComponent = () => {
+  return <p className="text-p">Hello World</p>;
+};
+
+export default HelloComponent;
---
+++ src/container/StatefullComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// statefull component
+class StatefullComponent extends React.Component {
+  render() {
+    return <p>Hello World statefull</p>;
+  }
+}
+
+export default StatefullComponent;
